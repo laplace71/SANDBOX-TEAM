f5add57c6b23cb18c3fd08e81da5484ed18b56db
Melany Quispe <melany.ql.2006@gmail.com>
2026-09-02 12:19:19 -0500

    feat: cambio mensaje musica

--- StudentModel.java
+++ StudentModel.java
@@ -1,1 +1,6 @@
-public class StudentModel {} 
+public class StudentModel {
+    public static void main(String[] args) {
+        String mensaje = "La música es bonita y nos alegra el día";
+        System.out.println(mensaje);
+    }
+}
